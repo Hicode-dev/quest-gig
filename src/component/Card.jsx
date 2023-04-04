5f8be049e6223847966e15519e1b31ea22772175
hicode-dev <kingdavies2021@gmail.com>
2023-04-04 16:22:51 -0700

    added some api functionalities to it

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -3,51 +3,53 @@ import image1 from '../assets/unsplash_4hbJ-eymZ1o.svg'
 import image2 from '../assets/unsplash_gKXKBY-C-Dk.png'
 import image3 from '../assets/unsplash_gKXKBY-C-Dk.svg'
 import {AiFillWechat,AiOutlineCloudDownload,AiFillCopy} from "react-icons/ai"
+import { useState,useEffect, } from 'react';
+import axios from 'axios';
 const Card = () => {
+  const [newsData, setNewsData] = useState([]);
+const Fetch= async () =>{
+  const config = {
+      
+    params: {
+      country: 'ng',
+      api_key: '65gh2-60GD-1742-QAPI',
+    
+    }
+  };
+
+  let {data} =await axios.get('https://questgig.com/api/v2/news?', config)
+  setNewsData(data.articles)
+  console.log(data.articles);
+}
+  useEffect(() => {
+   Fetch()
+  }, []);
+
+ 
+
+
   return (
    <div className='flex capitalize flex-wrap  justify-center items-center gap-5'>
-     <div className=' border mt-3 px-3 shadow-2xl bg-gray-100'>
-      <div >
-        <h5 className='py-3'>what a cat eat for sure</h5>
-            <p>3 mins</p>
-      </div>
-      <div className="imaoge p-3">
-        <img src={image2} alt="" />
-      </div>
-        <div className="icons flex px-3 justify-between items-center">
-            <AiFillWechat  className='text-2xl '  />
-            <AiOutlineCloudDownload  className='text-2xl ' />
-            <AiFillCopy  className='text-2xl ' />
+    {
+      newsData.map((C)=>(
+        <div className=' border mt-3 px-3 shadow-2xl bg-gray-100'>
+        <div >
+          <h5 className='py-3'>{C.title}</h5>
+              <p>3 mins</p>
         </div>
-    </div>
-     <div className=' border mt-3 px-3 shadow-2xl bg-gray-100'>
-      <div >
-        <h5 className='py-3'>what a cat eat for sure</h5>
-            <p>3 mins</p>
-      </div>
-      <div className="imaoge p-3">
-        <img src={image2} alt="" />
-      </div>
-        <div className="icons flex px-3 justify-between items-center">
-            <AiFillWechat  className='text-2xl '  />
-            <AiOutlineCloudDownload  className='text-2xl ' />
-            <AiFillCopy  className='text-2xl ' />
+        <div className="image p-3">
+          <img src={image2} alt="" />
         </div>
-    </div>
-     <div className=' border mt-3 px-3 shadow-2xl bg-gray-100'>
-      <div >
-        <h5 className='py-3'>what a cat eat for sure</h5>
-            <p>3 mins</p>
+          <div className="icons flex px-3 justify-between items-center">
+              <AiFillWechat  className='text-2xl '  />
+              <AiOutlineCloudDownload  className='text-2xl ' />
+              <AiFillCopy  className='text-2xl ' />
+          </div>
       </div>
-      <div className="imaoge p-3">
-        <img src={image2} alt="" />
-      </div>
-        <div className="icons flex px-3 justify-between items-center">
-            <AiFillWechat  className='text-2xl '  />
-            <AiOutlineCloudDownload  className='text-2xl ' />
-            <AiFillCopy  className='text-2xl ' />
-        </div>
-    </div>
+      ))        
+    
+    }
+   
 
   
    </div>
---
+++ src/component/CardCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CardCard = ({
+id,
+title
+
+}) => {
+  return (
+    <div>
+      {title}
+    </div>
+  )
+}
+
+export default CardCard
