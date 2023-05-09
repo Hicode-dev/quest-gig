aa711c9b8ada11a7d9cca3f903668f5255bc15dd
hicode-dev <kingdavies2021@gmail.com>
2023-05-09 01:33:49 -0700

    lop

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -2,62 +2,59 @@ import React from 'react';
 import image1 from '../assets/unsplash_4hbJ-eymZ1o.svg'
 import image2 from '../assets/unsplash_gKXKBY-C-Dk.png'
 import image3 from '../assets/unsplash_gKXKBY-C-Dk.svg'
-
-import {AiFillWechat,AiOutlineCloudDownload,AiFillCopy} from "react-icons/ai"
-import { useState,useEffect, } from 'react';
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+import { AiFillWechat, AiOutlineCloudDownload, AiFillCopy } from "react-icons/ai"
+import { useState, useEffect, } from 'react';
 import axios from 'axios';
+import CardCard from './CardCard';
 const Card = () => {
   const [newsData, setNewsData] = useState([]);
   const [numArticles, setNumArticles] = useState(9
-);
-const Fetch= async () =>{
-  const config = {
-      
-    params: {
-      country: 'ng',
-      api_key: '65gh2-60GD-1742-QAPI',
-      q:  Math.floor(Math.random(numArticles) * 100)
-    
-    }
-  };
-  let {data} =await axios.get('https://questgig.com/api/v2/news?q=10', config)
-  setNewsData(data.articles.slice(0, numArticles))
-  console.log(data.articles);
-}
+  );
+  const Fetch = async () => {
+    const config = {
+
+      params: {
+        country: 'ng',
+        api_key: '65gh2-60GD-1742-QAPI',
+        q: Math.floor(Math.random(numArticles) * 36)
+
+      }
+    };
+    let { data } = await axios.get('https://questgig.com/api/v2/news?q=10', config)
+    setNewsData(data.articles.slice(0, numArticles))
+    console.log(data.articles);
+  }
   useEffect(() => {
-   Fetch()
+    Fetch()
+    Aos.init({
+      duration: 1000,
+      useClassNames: true
+    })
   }, []);
 
- 
 
 
   return (
-   <div className='flex capitalize flex-wrap  justify-center items-center gap-5'>
-    {
-      newsData.map((C)=>(
-        <div className=' border mt-3 px-3 shadow-2xl bg-gray-100 md:w-1/4 h-3/4'>
-        <div >
-          <h5 className='py-3'>{C.title}</h5>
-              <p>3 mins</p>
-        </div>
-        <div className="image p-3">
-          <img src={image2} alt="" />
-        </div>
-        <h5 className='py-3'>{C.description}  <a href={C.url} className='text-blue-700'>see more</a></h5>
+    <div className='flex capitalize flex-wrap  justify-center animation  gap-5' data-aos="zoom-out-left">
+      {
+        newsData.map((C) => (
+          <CardCard
+            title ={C.title}
+            image ={C.news_thumbnail            }
+            alt = 'image'
+            des ={C.description
+            }
+            href ={C.url}
+          />
+        ))
 
-          <div className="icons flex px-3 justify-between items-center">
-              <AiFillWechat  className='text-2xl '  />
-              <AiOutlineCloudDownload  className='text-2xl ' />
-              <AiFillCopy  className='text-2xl ' />
-          </div>
-      </div>
-      ))        
-    
-    }
-   
+      }
 
-  
-   </div>
+
+
+    </div>
   );
 }
 
--- src/component/CardCard.jsx
+++ src/component/CardCard.jsx
@@ -1,14 +1,40 @@
 import React from 'react'
-
+import { AiFillWechat, AiOutlineCloudDownload, AiFillCopy } from "react-icons/ai"
+import { useEffect } from 'react'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 const CardCard = ({
-id,
-title
-
+  title,
+  image,
+  alt,
+  des,
+ href,
 }) => {
+  useEffect(() => {
+    Aos.init({
+      duration: 1000,
+      useClassNames: true
+    })
+  }, []);
   return (
-    <div>
-      {title}
+
+  <div className=' border mt-3 px-3 shadow-2xl bg-gray-100 md:w-1/4  py-3'   data-aos="zoom-out-down">
+    <div >
+      <h5 className='py-3'>{title}</h5>
+      <p>3 mins</p>
     </div>
+    <div className="image p-3">
+      <img src={image
+      } alt='image' />
+    </div>
+    <h5 className='py-3'>{des}  <a href={href} className='text-blue-700' target='_blank'>see more</a></h5>
+
+    <div className="icons flex px-3 justify-between items-center">
+      <AiFillWechat className='text-2xl ' />
+      <AiOutlineCloudDownload className='text-2xl ' />
+      <AiFillCopy className='text-2xl ' />
+    </div>
+  </div>
   )
 }
 
