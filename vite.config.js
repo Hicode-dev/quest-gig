50069fd9b26a3cfb1816e3469e221b5aa64e5036
hicode-dev <kingdavies2021@gmail.com>
2023-05-09 01:51:24 -0700

    this

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+    build: {
+    rollupOptions: {
+      external: ['react','react-dom','react-icons'] // Add 'react-icons' to the external array
+    },
+   
+  },
+
 })
