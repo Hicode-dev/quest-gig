5b12b3473a1ff97a0314178779da25967242ff88
hicode-dev <kingdavies2021@gmail.com>
2023-05-09 13:06:05 -0700

    po

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  import react from 'react';
-
-export default {
   build: {
     rollupOptions: {
       external: [
         'react-icons/bs',
+        'react-icons/ri',
       ],
     },
   },
-};
-
 })
